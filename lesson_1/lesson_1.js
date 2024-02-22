a9b48c1befff583fa3e6f60507d6b0331194b237
Medovik <42012233+Medovik9696@users.noreply.github.com>
2024-02-23 00:04:53 +0100

    Second

---
+++ lesson_1/lesson_1.js
@@ -0,0 +1,6 @@
+function droidsOrder(quantity, pricePerDroid) {
+    let totalPrice = quantity * pricePerDroid;
+    console.log("Your ordered " + quantity + " droids worth " + totalPrice + " credits");
+};
+droidsOrder(3, 100);
+
