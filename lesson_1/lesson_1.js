bc3a9e28399ce776faa40a30d8c439dced59a08a
Medovik <42012233+Medovik9696@users.noreply.github.com>
2024-02-23 10:50:06 +0100

    Update lesson_1.js

--- lesson_1/lesson_1.js
+++ lesson_1/lesson_1.js
@@ -4,3 +4,21 @@ function droidsOrder(quantity, pricePerDroid) {
 };
 droidsOrder(3, 100);
 
+console.log();
+
+function getShippingMessage(country, price, deliveryFee) { 
+    let totalPrice = price + deliveryFee;
+    console.log("Shiping to " + country + " will cost " + totalPrice + " credits");
+}
+getShippingMessage("USA", 1500, 120);
+
+console.log();
+
+function getElementWidth(content, pending, border) { 
+    content = parseInt(content);
+    pending = parseInt(pending);
+    border = parseInt(border);
+    result = content + pending * 2 + border * 2;
+    console.log("Element width =  " + result);
+};
+getElementWidth("50px","8px","4px");
